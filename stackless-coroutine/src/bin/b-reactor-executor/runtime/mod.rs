9a83bc27c07c12f0024b37dfc958bc3bb437ffbd
John Arumemi-Ikhide <john.arumemi@gmail.com>
2025-01-19 15:36:33 +0000

    feat(b-reactor-executor)!: changed project structure to reflect new design

--- stackless-coroutine/src/bin/b-reactor-executor/runtime/mod.rs
+++ stackless-coroutine/src/bin/b-reactor-executor/runtime/mod.rs
@@ -12,64 +12,10 @@ use crate::future::{Future, PollState};
 mod executor;
 mod reactor;
 
-/// Registry is used for registering interest in events on a source.
-///
-/// When HttpGetFuture makes a non-blocking IO request, it should
-/// register interest on read events on the streams file descriptor.
-///
-/// # OnceLock<T>
-/// OnceLock is used to ensure static can only be written to once.
-/// Useful for Singletons.
-pub static REGISTRY: OnceLock<Registry> = OnceLock::new();
+pub use executor::{spawn, Executor, Waker};
+pub use reactor::reactor;
 
-pub fn registry() -> &'static Registry {
-    // we expect the runtime, on initialisation, to set the REGISTRY static variable.
-    REGISTRY
-        .get()
-        .expect("Registry not initialized. Called outside a runtime context.")
-}
-
-pub struct Runtime {
-    /// Abstraction over the OS'es event_queue polling / select interface
-    poll: Poll,
-}
-
-impl Runtime {
-    pub fn new() -> Self {
-        // create a new poll instance and also the underlying OS event queue.
-        let poll = Poll::new().unwrap();
-
-        // get a clone to the poll's registry to set global registry.
-        // This is now a registry handle owned by the runtime!
-        let registry = poll.registry().try_clone().unwrap();
-
-        // set the global REGISTRY static variable
-        REGISTRY
-            .set(registry)
-            .expect("Failed to set REGISTRY static variable");
-
-        Self { poll }
-    }
-    /// The `block_on` method is used to run the future to completion.
-    ///
-    /// It represents the original `main` function in the `a-coroutine` example.
-    /// NOTE: this implementation does not support multiple top-level futures.
-    pub fn block_on<F>(&mut self, mut future: F)
-    where
-        // corofy only supports futures resolving with strings
-        F: Future<Output = String>,
-    {
-        // Remember, out top-level future will return Ready only when all child futures have
-        // resolved and return PollState::Ready.
-        while let PollState::NotReady = future.poll() {
-            println!("\nCurrent future is not ready. Schedule other tasks.");
-
-            // rather than sleep, we block on the event_queue (epoll or kqueue syscalls) with no
-            // timeout specified. It is the responsibility of HttpGetRequest to ensure
-            // it registers interest on a source when it makes a non-blocking IO request.
-            let mut events = Events::with_capacity(100);
-            self.poll.poll(&mut events, None);
-            println!("Woken up from poll. Checking for ready tasks.\n");
-        }
-    }
+pub fn init() -> Executor {
+    reactor::start();
+    Executor::new()
 }
